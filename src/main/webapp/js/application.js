d7c65ced2152899eeff06830c53c79077bcc86c2
Christian Graver <christiangraver@gmail.com>
2013-08-22 17:12:42 +0200

    playing with styles

---
+++ src/main/webapp/js/application.js
@@ -0,0 +1,24 @@
+// NOTICE!! DO NOT USE ANY OF THIS JAVASCRIPT
+// IT'S ALL JUST JUNK FOR OUR DOCS!
+// ++++++++++++++++++++++++++++++++++++++++++
+
+!function ($) {
+
+  $(function(){
+
+    var $window = $(window)
+    var $body   = $(document.body)
+
+    var navHeight = $('.navbar').outerHeight(true) + 10
+
+    $body.scrollspy({
+      target: '.bs-sidebar',
+      offset: navHeight
+    })
+
+    $window.on('load', function () {
+      $body.scrollspy('refresh')
+    })
+})
+
+}(window.jQuery)
