b22537d794cbdc146ed6b6520c345476cdf1c9ec
Christian Graver <christiangraver@gmail.com>
2013-10-04 14:14:53 +0200

    Validation on time_selection

--- src/main/webapp/app/time-selection.js
+++ src/main/webapp/app/time-selection.js
@@ -2,7 +2,10 @@ function timeSelection($scope,UrlService) {
 
     $scope.timeZones = [
         {ID: 'utc', Title: 'UTC'},
-        {ID: 'utc/gmt+1', Title: 'UTC/GMT+1'}
+        {ID: 'utc/gmt+1', Title: 'UTC/GMT+1'},
+        {ID: 'utc/gmt+2', Title: 'UTC/GMT+2'},
+        {ID: 'utc/gmt+3', Title: 'UTC/GMT+3'},
+        {ID: 'utc/gmt+4', Title: 'UTC/GMT+4'}
     ];
 
     // Datepicker directive
@@ -14,6 +17,8 @@ function timeSelection($scope,UrlService) {
 
     $scope.startTimeZone = {zone: 'utc'};
     $scope.endTimeZone = {zone: 'utc'};
+
+    $scope.datepicker = {date: new Date("2012-09-01T00:00:00.000Z"), setStartDate: new Date('2012-08-01T00:00:00.000Z')};
 
 
     //Watch for updates
@@ -69,5 +74,54 @@ function timeSelection($scope,UrlService) {
         //Send to service
         UrlService.setToDate(toDate);
     }
+    //
+    //Validation of dates
+    //
+    //Check if date is en future
+    $scope.checkIfFutureDate = function() {
+        var startDate = new Date($scope.startDatepicker.date);
+        var currentDate = new Date();
 
+        console.log('check date: input: '+startDate+'now:'+currentDate);
+
+
+
+        //Making boolean to control css-class
+        //We want only past dates
+        if (currentDate<startDate) {
+            console.log('future');
+            return true;
+        }
+        else {
+            console.log('past');
+            return false;
+        }
+    }
+    //Check if end date is after start date
+    $scope.afterStartDate = function() {
+        var startDate = new Date($scope.startDatepicker.date)
+        var endDate = new Date($scope.endDatepicker.date);
+        //var currentDate = new Date();
+
+        //console.log('check date: input: '+inputDate+'now:'+currentDate);
+
+
+
+        //Making boolean to control css-class
+        //We want only past dates
+        if (startDate>endDate) {
+            console.log('startDate after endDate :(');
+            return true;
+        }
+        else {
+            console.log('startDate before endDate :)');
+            return false;
+        }
+    }
+    $scope.returnNowDate = function() {
+        //angular strap will not accept dynamic dates for start/end dates
+        //nowdate will be today no matter what it is assigned to
+        datedate = new Date();
+        return datedate;
+    }
 }
