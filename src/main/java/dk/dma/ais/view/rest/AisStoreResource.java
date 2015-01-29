5197bca9c2abd830437a2eedeac57cff6c3b7490
Jens Tuxen <mail@jenstuxen.com>
2015-01-29 11:17:34 +0100

    change fetchsize for area to something reasonable

--- src/main/java/dk/dma/ais/view/rest/AisStoreResource.java
+++ src/main/java/dk/dma/ais/view/rest/AisStoreResource.java
@@ -187,7 +187,7 @@ public class AisStoreResource extends AbstractResource {
         } else if (p.getArea() != null) {
             b = AisStoreQueryBuilder.forArea(p.getArea());
             b.setFetchSize(QueryParameterValidators.getParameterAsInt(info,
-                    "fetchSize", 2));
+                    "fetchSize", 512));
 
         } else {
             b = AisStoreQueryBuilder.forTime();
